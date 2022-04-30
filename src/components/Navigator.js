754c8060c3d4e2048a0fbc12d2fe3406fac7f7ef
Valentin Schöneich <schoeneich@sbmc-solutions.com>
2022-04-30 22:01:54 +0200

    Login Popup

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,19 +1,36 @@
 import React,  { useState } from 'react';
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import Toast from 'react-bootstrap/Toast';
 import Button from 'react-bootstrap/Button';
+import LoginPopUp from "./LoginPopUp";
 
 
 const Navigator = props => {
-    const [show, toggleShow] = useState(true);
+    const [loginPopUp, setLoginPopUp] = useState(false);
+    const [registrationPopUp, setRegistrationPopUp] = useState(false);
+
+    const switchLoginPopUp = () => {
+        setLoginPopUp(show => !show);
+    }
+
+    const switchRegistrationPopUp = () => {
+        setRegistrationPopUp(show => !show);
+    }
+
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" >
                 <Container>
                     <Navbar.Brand href="/">Rent-a-car</Navbar.Brand>
-                    {/*<Nav className="me-auto">
+                    <Nav className="me-auto">
                         <Nav.Link href="">Auto mieten</Nav.Link>
-                    </Nav>*/}
+                    </Nav>
+                    <Button variant="link" onClick={switchLoginPopUp}>Login</Button>
+                    {loginPopUp
+                        ? <LoginPopUp showPopUp={loginPopUp} switchPopUp={switchLoginPopUp}
+                                 uuid={props.uuid} pid={props.pid} perf={props.perf}/>
+                        : null}
+                    <Navbar.Text>|</Navbar.Text>
+                    <Button variant="link" onClick={switchRegistrationPopUp}>Registierung</Button>
                 </Container>
             </Navbar>
         </>
