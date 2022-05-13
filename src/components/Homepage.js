5cec9275177cb04fa7081eaa36057752f5847c37
vgehr001 <vincent.gehrmann@student.hs-rm.de>
2022-05-13 14:42:39 +0200

    search and date

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,37 @@
 import React from 'react';
-
-
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 const Homepage = props => {
-
+    state object datum ort json
     return (
         <div>
             <h1>Homepage</h1>
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Ort"
+                    className="me-2"
+                    aria-label="Search"
+                    onchange=
+                />
+                <FormControl
+                    type="date"
+                    placeholder="Von"
+                    max="2023-12-31"
+                    id="von"
+                    onchange=
+                />
+                <FormControl
+                    type="date"
+                    placeholder ="Bis"
+                    max="2023-12-31"
+                    onchange=
+                />
+                <Button variant="outline-success">Suche</Button>
+            </Form>
         </div>
-    );
+    );  
 }
 
 export default Homepage;
