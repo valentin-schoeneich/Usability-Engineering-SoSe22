46f33bf3b999bd3794877ffc30a7a7e20d9a5d17
vgehr001 <vincent.gehrmann@student.hs-rm.de>
2022-05-17 22:48:43 +0200

    car-card

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,35 +1,58 @@
-import React from 'react';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React, { useState} from 'react';
 const Homepage = props => {
-    state object datum ort json
+    const [searchData, setSearchData] = useState({"location": "", "startDate": "", "endDate":""});
+    const handleChange = event => {
+        setSearchData({...searchData, [event.target.name]: event.target.value});
+    }
+    const handleSubmit = event => {
+        console.log(searchData.location);
+        window.location="http://localhost:3000/f40";
+
+    }
+    const handleRent = event => {
+        window.location="http://localhost:3000/f40";
+    }
     return (
         <div>
             <h1>Homepage</h1>
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={handleSubmit} >
                 <FormControl
                     type="search"
                     placeholder="Ort"
                     className="me-2"
                     aria-label="Search"
-                    onchange=
+                    name="location"
+                    onChange= {handleChange}
                 />
                 <FormControl
                     type="date"
                     placeholder="Von"
+                    min="2022-05-18"
                     max="2023-12-31"
-                    id="von"
-                    onchange=
+                    name="startDate"
+                    onChange= {handleChange}
                 />
                 <FormControl
                     type="date"
                     placeholder ="Bis"
+                    min="2022-05-18"
                     max="2023-12-31"
-                    onchange=
+                    name="endDate"
+                    onChange= {handleChange}
                 />
-                <Button variant="outline-success">Suche</Button>
+                <Button variant="outline-success" type="submit">Suche</Button>
             </Form>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://cdn.motor1.com/images/mgl/3zoJ6/s1/4x3/ferrari-feiert-30-jahre-f40.webp" />
+                <Card.Body>
+                    <Card.Title>Ferrari F40</Card.Title>
+                    <Button variant="primary" onClick={handleRent}>Jetzt Mieten</Button>
+                    </Card.Body>
+            </Card>
         </div>
     );  
 }
