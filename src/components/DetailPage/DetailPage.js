c23ae67a3b552d2b67cf2f8b50e4a99c9d288766
noah54321 <nlanger781@gmail.com>
2022-05-25 19:56:53 +0200

    scroll for more Details calculate final Price function

--- src/components/DetailPage/DetailPage.js
+++ src/components/DetailPage/DetailPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import car from "../../imgs/car.png";
 import car2 from "../../imgs/car2.png";
 import arrow from "../../imgs/arrow.svg"
-import {Button, Col, Container, Row, NavLink} from "react-bootstrap";
+import {Button, Col, Container, NavLink, Row} from "react-bootstrap";
 import ImageGallery from 'react-image-gallery';
 
 
@@ -17,6 +17,19 @@ function showHiddeDetails(){
         document.getElementById("moreDetailText").textContent="Weniger Details anzeigen";
         document.getElementById("arrow").style.transform = "rotate(0deg)";
     }
+}
+
+function calculateFinalPrice(){
+    let pricePerDay = parseFloat(document.getElementById("pricePerDay").textContent);
+    let deposit = parseFloat(document.getElementById("deposit").textContent);
+
+    let from = new Date(document.getElementById("from").textContent);
+    let to = new Date(document.getElementById("to").textContent);
+
+    let days = ((to-from)/1000/60/60/24)+1;
+    console.log(days)
+
+    document.getElementById("finalPrice").textContent = pricePerDay * days + deposit;
 }
 
 
@@ -44,32 +57,51 @@ const DetailPage = props => {
                 <Col>
                     <h2>Details</h2>
                     <ul>
-                        <li>Türen: 5</li>
-                        <li>Sitze: 5</li>
-                        <li>Schatung: manuel</li>
-                        <li>S-Klasse Merceds</li>
+                        <li>Türen: <span>5</span></li>
+                        <li>Sitze: <span>5</span></li>
+                        <li>Schatung: <span>manuel</span></li>
+                        <li><p>S-Klasse Merceds</p></li>
                     </ul>
-                    <NavLink onClick={showHiddeDetails}><span id="moreDetailText">Alle Details anzeigen</span><img id="arrow" src={arrow} alt={"arrow"}/></NavLink>
-                    <ul id="unimportantDetails">
+                    <NavLink href={"#unimportantDetails"}><span id="moreDetailText">Zu allen Details</span><img id="arrow" src={arrow} alt={"arrow"}/></NavLink>
+                </Col>
+                <Col>
+                    <div id="bordered">
+                        <h2>Preis</h2>
+                        <ul >
+                            <li>€/Tag: <span id="pricePerDay">50</span>€</li>
+                            <li>Kautoin: <span id={"deposit"} >200</span>€</li>
+                            <li>Kilometerlimit: <span>100</span>km</li>
+                        </ul>
+                        <div id={"summary"}><span id={"finalPrice"}>X</span>€ (inkl. Kaution) vom <span id={"from"}>11.11.2022</span> bis <span id={"to"}>11.12.2022</span></div>
+                        <Button id={"bookButton"} onClick={calculateFinalPrice}>Jetzt Buchen (Test zur Preisberechnung)</Button>
+                    </div>
+                </Col>
+            </Row>
+            <Row >
+                <Col>
+                    <ul id={"unimportantDetails"}>
                         <li>mit Navi</li>
                         <li>mit Klimaanlage</li>
                         <li>mit Infotainment</li>
                         <li>unwichtig: 500</li>
                         <li>unwichtig: 500</li>
                         <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
+                        <li>unwichtig: 500</li>
                     </ul>
-                </Col>
-                <Col>
-                    <div id="bordered">
-                        <h2>Preis</h2>
-                        <ul>
-                            <li>€/Tag: </li>
-                            <li>Kautoin: </li>
-                            <li>Kilometerlimit: </li>
-                        </ul>
-                        <p>x€ für dem Zeitraum von x - x</p>
-                        <Button>Jetzt Buchen</Button>
-                    </div>
                 </Col>
             </Row>
         </Container>
