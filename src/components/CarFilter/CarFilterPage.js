2a7ead02ecfc8881847671db305b8518e03357c5
Valentin Schöneich <schoeneich@sbmc-solutions.com>
2022-05-26 11:16:03 +0200

    CarFilterPage

---
+++ src/components/CarFilter/CarFilterPage.js
@@ -0,0 +1,13 @@
+import React,  { useState } from 'react';
+
+const CarFilterPage = props => {
+
+
+    return (
+        <>
+            <h1>Filter</h1>
+        </>
+    );
+}
+
+export default CarFilterPage;
