7d7677cb48e05ede8d7f01715d889bbc451bdbfa
Valentin Schöneich <schoeneich@sbmc-solutions.com>
2022-05-29 22:22:59 +0200

    CarFilterPage expanded with filter and car tiles section

--- src/components/CarFilter/CarFilterPage.js
+++ src/components/CarFilter/CarFilterPage.js
@@ -1,11 +1,35 @@
 import React,  { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import { useParams } from "react-router-dom"
+import Filter from "./Filter";
+import Col from "react-bootstrap/cjs/Col";
+import CarTile from "./CarTile";
+import Container from "react-bootstrap/cjs/Container";
+import {Row} from "react-bootstrap";
+
 
 const CarFilterPage = props => {
+    const [carsData, setCarsData] = useState(JSON.parse(window.localStorage.getItem("carsData")));
+    const [filteredCars, setFilteredCars] = useState(carsData);
+    const { location, startDate, endDate } = useParams();
 
 
     return (
         <>
-            <h1>Filter</h1>
+            <h1>CarFilterPage</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <Filter carsData={carsData} filteredCars={filteredCars} setFilteredCars={setFilteredCars}
+                            location={location} startDate={startDate} endDate={endDate}/>
+                    </Col>
+                    <Col>
+                        {filteredCars.map((car, key) => {
+                            return <CarTile car={car} key={key}/>;
+                        })}
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
