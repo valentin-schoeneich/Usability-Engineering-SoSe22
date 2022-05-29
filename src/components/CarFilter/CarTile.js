3632b8084a022e012c0f7195e394317792765149
Valentin Schöneich <schoeneich@sbmc-solutions.com>
2022-05-29 22:23:14 +0200

    Car tiles component

---
+++ src/components/CarFilter/CarTile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Card} from "react-bootstrap";
+
+
+const CarTile = props => {
+
+
+    return (
+        <>
+            <Card style={{ width: '30rem' }}>
+                <Card.Header>any car</Card.Header>
+                <Card.Body>
+                    any cars details<br/>
+                    -<br/>
+                    -<br/>
+                    -<br/>
+                    -<br/>
+                    -<br/>
+                </Card.Body>
+            </Card>
+        </>
+    );
+}
+
+export default CarTile;
