2083aba55fff54cb4d5beb79483d380411b9e295
noah54321 <nlanger781@gmail.com>
2022-06-15 18:28:23 +0200

    Anmelden/angemeldet sein auf Bookingpage lässt Eingaben verschwinden Detailsseite mit Icons; Preis Box auf Style der andern Seiten angepasst Footer minHeight dynamisch setzen um große Scrollbare Lücke auf Bookingpage zu verhindern

--- src/components/homepage/SearchFields.js
+++ src/components/homepage/SearchFields.js
@@ -50,10 +50,17 @@ const SearchFields = props => {
     }
 
     function changeDateToTextInput(target){
-        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-        let date = new Date(target.value);
         target.type = "text";
-        target.value = date.toLocaleDateString('de-DE', options);
+        target.value = helper(target.value)
+    }
+
+    function helper(value){
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+        let date = new Date(value);
+        if(date.getTime() === date.getTime()){
+            return date.toLocaleDateString('de-DE', options);
+        }
+        return "";
     }
 
     function changeTextToDateInput(target){
@@ -61,6 +68,7 @@ const SearchFields = props => {
             let dateString = target.value.substr(6,4) + "-" + target.value.substr(3,2) + "-" + (target.value.substr(0,2))
             target.type = "date";
             target.value = dateString;
+            target.showPicker()
         }
     }
 
@@ -98,7 +106,7 @@ const SearchFields = props => {
                         onBlur={(e) => (changeDateToTextInput(e.target))}
                         max="2023-12-31"
 
-                        defaultValue={props.valueStartDate ? props.valueStartDate : ""}
+                        defaultValue={props.valueStartDate ? helper(props.valueStartDate) : ""}
                         name="startDate"
                         id = "von"
                         isInvalid={!!errors.startDate}
@@ -117,7 +125,7 @@ const SearchFields = props => {
                         onBlur={(e) => (changeDateToTextInput(e.target))}
                         min= {startD?startD.value:todayDate}
                         max="2023-12-31"
-                        defaultValue={props.valueEndDate ? props.valueEndDate : ""}
+                        defaultValue={props.valueEndDate ? helper(props.valueEndDate) : ""}
                         name="endDate"
                         id="bis"
                         onChange= {handleChange}
